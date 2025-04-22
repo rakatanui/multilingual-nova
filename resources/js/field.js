408d27edbfc580b5a19aad9d7691cacd186d8abe
Andrei Krauchanka <andrei.krauchanka@itdep.net>
2025-04-22 21:00:45 +0300

    Upgrade to Vue 3 and refactor Nova booting logic.
    Migrated from Vue 2 to Vue 3, updating relevant dependencies and syntax in `package.json`. Refactored the Nova booting function to use Vue 3's application instance (`app`) and updated router handling to `addRoute` for compatibility.

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,37 +1,33 @@
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-import IndexField from './components/IndexField.vue';
-import DetailField from './components/DetailField.vue';
-import FormField from './components/FormField.vue';
-import LanguageSelector from './components/LanguageSelector.vue';
-import Tool from './components/Tool.vue';
+import IndexField from './components/IndexField.vue'
+import DetailField from './components/DetailField.vue'
+import FormField from './components/FormField.vue'
+import LanguageSelector from './components/LanguageSelector.vue'
+import Tool from './components/Tool.vue'
 
-Nova.booting((Vue, router) => {
-    
-    Vue.component('index-multilingual-nova', IndexField);
-    Vue.component('detail-multilingual-nova', DetailField);
-    Vue.component('form-multilingual-nova', FormField);
+Nova.booting((app, router) => {
+    app.component('index-multilingual-nova', IndexField)
+    app.component('detail-multilingual-nova', DetailField)
+    app.component('form-multilingual-nova', FormField)
+    app.component('language-selector', LanguageSelector)
 
-    Vue.component('language-selector', LanguageSelector);
+    router.addRoute({
+        name: 'nova-language-tool',
+        path: '/nova-language-tool',
+        component: Tool,
+    })
 
-    router.addRoutes([
-        {
-            name: 'nova-language-tool',
-            path: '/nova-language-tool',
-            component: Tool,
-        },
-    ]);
-    
-    let lang = getParameterByName('lang');
+    let lang = getParameterByName('lang')
     if (lang) {
-        Nova.request().defaults.headers['lang'] = lang;
+        Nova.request().defaults.headers['lang'] = lang
     }
 })
